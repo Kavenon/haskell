d6969053f27e424a51d142b461021aad69f6fd8a
kavenon <kavenon@gmail.com>
2015-05-18 00:15:55 +0200

    data types and print

--- chessDataTypes.hs
+++ chessDataTypes.hs
@@ -1,12 +1,17 @@
 import Data.Char
+import System.Environment
+import Control.Monad
+import Data.Set
+import System.IO  
+import Control.Monad
 
 data Kolor = Bialy | Czarny deriving (Eq)
 data Bierka = Krol | Hetman | Pionek | Skoczek | Goniec | Wieza
 data Pole = Empty | Pole Bierka Kolor
 
-
-data Szachownica = Szachownica [[Pole]] -- odradzane, lepiej zrobic osobny podtyp
-
+data SzachownicaCol = SzachownicaCol Pole
+data SzachownicaRow = SzachownicaRow [SzachownicaCol]
+data Szachownica = Szachownica [SzachownicaRow]
 
 changeBierkaToChar Krol = 'K'
 changeBierkaToChar Hetman = 'Q'
@@ -21,17 +26,40 @@ poleToChar (Pole bierka kolor)
 	| kolor == Czarny = toLower bierkaChar
 	where bierkaChar = changeBierkaToChar bierka
 
+c = SzachownicaCol (Pole Krol Bialy )
+a = SzachownicaCol (Pole Krol Czarny )
+b = SzachownicaRow [c,a]
+
+szach = Szachownica [b]
+
+wyswietlCol (SzachownicaCol a) = charToString(poleToChar a);
+
+wyswietlRow (SzachownicaRow []) = "\n"
+wyswietlRow (SzachownicaRow (a:as)) =  wyswietlCol a ++ (wyswietlRow (SzachownicaRow as))
+
+wyswietlSzachownica (Szachownica []) = "\nkoniec szachownicy \n"
+wyswietlSzachownica (Szachownica (a:as)) = wyswietlRow a ++ (wyswietlSzachownica(Szachownica as))
 
 
+{- read from file -}
 
+t = readFile "plansza"
 
-{-
-instance Show Bierka where
-                show Krol = "Krol"
+{-instance Show SzachownicaCol where
+  show (SzachownicaCol (Pole bierka kolor)) = charToString (poleToChar(Pole bierka kolor) )
+
+instance Show SzachownicaRow where
+  show (SzachownicaRow a) = show a-}
               
-               
-instance Show PionGracza where
+{-  instance Show PionGracza where
                 show Empty = "Empty"
                 show (Pion bierka kolor) = "Figura: " ++ show bierka -}
                
+
+
+charToString :: Char -> String
+charToString = (:[])
+
+
+
 
